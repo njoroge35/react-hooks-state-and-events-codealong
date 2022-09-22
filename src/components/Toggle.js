5d110e012d0d1c1de3222bf9bcc8e761fa56d103
David <david.njoroge@student.moringaschool.com>
2022-09-22 19:39:52 +0300

    react-hooks-state-and-events-codealong

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react"; 
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn] = useState(false)
+
+  function handleClick() {
+    // updating state directly is a no-no!
+    setIsOn((isOn) => !isOn);
+  } 
+
+
+  return <button  style={{ background: "red" }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
